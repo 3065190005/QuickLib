7b07367ff669d59820c991f56cbc7edcf4b33695
DevilFish <52348258+3065190005@users.noreply.github.com>
2022-09-30 16:58:56 +0800

    Delete QuickLib directory

--- QuickLib/quicklib.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef _CERVER_QUICKLIB_
-#define _CERVER_QUICKLIB_
-
-#include "string.h"
-#include "encrypt.h"
-
-#endif
--- QuickLib/string.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifndef _CERVER_STRING_
-#define _CERVER_STRING_
-
-#include "kString/kString.h"
-namespace kstring = Cervice::Obj::String;
-
-#endif
